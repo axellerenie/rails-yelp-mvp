762126fdfc0fa6495f2abe3f314e8de7c9eaec07
Axelle Renie <axelle.renie@gmail.com>
2017-03-08 15:28:24 +0100

    suite

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'top', to: 'restaurants#top'
+    end
+    member do
+      get 'chef', to: 'restaurants#chef'
+    end
+  resources :reviews, only:[:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:          "Epicure au Bristol",
+    address:       "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "0145224289",
+    category:      "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "0145224289",
+    category:      "french"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "0145224289",
+    category:      "french"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
+
+puts "Seed OK"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
